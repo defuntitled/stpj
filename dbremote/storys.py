ccc31e4a77a36f5d7093cba556c68ad52a184efc
kirill <kirya.smart.03@gmail.com>
2020-04-20 16:11:08 +0700

    created login and register

--- dbremote/storys.py
+++ dbremote/storys.py
@@ -12,4 +12,7 @@ class Story(SqlAlchemyBase):
     user_id = sqlalchemy.Column(sqlalchemy.Integer,
                                 sqlalchemy.ForeignKey("authors.id"))
     user = orm.relation('Author')
-    template_path = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    cover = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    head = sqlalchemy.Column(sqlalchemy.String, nullable=True)
