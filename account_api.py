0440337afd7b92a37bd04809b3c07e3d112cb303
kirill <kirya.smart.03@gmail.com>
2020-04-23 21:59:41 +0700

    login and register working

--- account_api.py
+++ account_api.py
@@ -15,7 +15,6 @@ import flask
 from werkzeug.security import generate_password_hash
 
 global_init("db/data.sqlite")
-session = create_session()
 
 blueprint = flask.Blueprint('news_api', __name__,
                             template_folder='templates')
@@ -49,82 +48,117 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('Please use a different email address.')
 
 
-@blueprint.route("/login", methods=["GET", "POST"])
-def login():
-    if current_user.is_authenticated:
-        return flask.redirect("/feed")
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = session.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return flask.redirect("/")
-        print(generate_password_hash(form.password.data))
-        return flask.render_template('login_template.html',
-                                     message="Неправильный логин или пароль",
+@blueprint.route("/login/<string:par>", methods=["GET", "POST"])
+def login(par):
+    if par == "user":
+        if current_user.is_authenticated:
+            return flask.redirect("/feed")
+        form = LoginForm()
+        if form.validate_on_submit():
+            session = create_session()
+            user = session.query(User).filter(User.email == form.email.data).first()
+            if user and user.check_password(form.password.data):
+                login_user(user, remember=form.remember_me.data)
+                return flask.redirect("/")
+            print(generate_password_hash(form.password.data))
+            return flask.render_template('login_template.html',
+                                         message="Неправильный логин или пароль",
+                                         form=form)
+        else:
+            print(form.errors)
+        return flask.render_template('login_template.html', action=False, title='Авторизация',
                                      form=form)
     else:
-        print(form.errors)
-    return flask.render_template('login_template.html', action=2, title='Авторизация', form=form)
-
-
-@blueprint.route("/login_author", methods=["GET", "POST"])
-def login_author():
-    if current_user.is_authenticated:
-        return flask.redirect("/dashboard")
-    form = LoginForm()
-    if form.validate_on_submit():
-        author = session.query(Author).filter(Author.email == form.email.data).first()
-        if author and author.check_password(form.password.data):
-            login_user(author, remember=form.remember_me.data)
+        if current_user.is_authenticated:
             return flask.redirect("/dashboard")
-        return flask.render_template('login_template.html',
+        form = LoginForm()
+        if form.validate_on_submit():
+            session = create_session()
+            author = session.query(Author).filter(Author.email == form.email.data).first()
+            if author and author.check_password(form.password.data):
+                login_user(author, remember=form.remember_me.data)
+                return flask.redirect("/dashboard")
+            return flask.render_template('login_template.html',
+                                         form=form)
+        else:
+            print(form.errors)
+        return flask.render_template('login_template.html', action=True, form=form)
+
+
+# @blueprint.route("/login_author", methods=["GET", "POST"])
+# def login_author():
+#     if current_user.is_authenticated:
+#         return flask.redirect("/dashboard")
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         author = session.query(Author).filter(Author.email == form.email.data).first()
+#         if author and author.check_password(form.password.data):
+#             login_user(author, remember=form.remember_me.data)
+#             return flask.redirect("/dashboard")
+#         return flask.render_template('login_template.html',
+#                                      form=form)
+#     else:
+#         print(form.errors)
+#     return flask.render_template('login_template.html', action=1, form=form)
+
+
+@blueprint.route('/register/<string:par>', methods=['GET', 'POST'])
+def register(par):
+    if par == "user":
+        if current_user.is_authenticated:
+            return flask.redirect("/feed")
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            print(form.username.data)
+            print(form.email.data)
+            print(form.password.data)
+            user = User()
+            user.nickname = form.username.data
+            user.email = form.email.data
+            user.hashed_password = generate_password_hash(form.password.data)
+
+            # добавить проверку email
+            session = create_session()
+            session.add(user)
+            print(type(form.username.data), type(form.email.data),
+                  type(generate_password_hash(form.password.data)))
+            session.commit()
+            return flask.redirect("/login")
+        return flask.render_template('registration_creator.html', action=2, title='Register',
                                      form=form)
     else:
-        print(form.errors)
-    return flask.render_template('login_template.html', action=1, form=form)
+        form = RegistrationForm()
+        if form.validate():
+            author = Author()
+            author.nickname = form.username.data
+            author.email = form.email.data
+            author.hashed_password = generate_password_hash(form.password.data)
+            session = create_session()
+            session.add(author)
+            session.commit()
+            os.mkdir(f"data/{author.id}")
+            return flask.redirect("/login")
+        else:
+            print(form.errors)
+        return flask.render_template('registration_creator.html', action=1, form=form)
 
 
-@blueprint.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return flask.redirect("/feed")
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        print(form.username.data)
-        print(form.email.data)
-        print(form.password.data)
-        user = User()
-        user.nickname = form.username.data
-        user.email = form.email.data
-        user.hashed_password = generate_password_hash(form.password.data)
-
-        # добавить проверку email
-        session = create_session()
-        session.add(user)
-        print(type(form.username.data), type(form.email.data),
-              type(generate_password_hash(form.password.data)))
-        session.commit()
-        return flask.redirect("/login")
-    return flask.render_template('registration_creator.html', action=2, title='Register', form=form)
-
-
-@blueprint.route("/register_author", methods=['GET', 'POST'])
-def new_author():
-    form = RegistrationForm()
-    if form.validate():
-        author = Author()
-        author.nickname = form.username.data
-        author.email = form.email.data
-        author.hashed_password = generate_password_hash(form.password.data)
-        session = create_session()
-        session.add(author)
-        session.commit()
-        os.mkdir(f"data/{author.id}")
-        return flask.redirect("/login")
-    else:
-        print(form.errors)
-    return flask.render_template('registration_creator.html', action=1, form=form)
+# @blueprint.route("/register_author", methods=['GET', 'POST'])
+# def new_author():
+#     form = RegistrationForm()
+#     if form.validate():
+#         author = Author()
+#         author.nickname = form.username.data
+#         author.email = form.email.data
+#         author.hashed_password = generate_password_hash(form.password.data)
+#         session = create_session()
+#         session.add(author)
+#         session.commit()
+#         os.mkdir(f"data/{author.id}")
+#         return flask.redirect("/login")
+#     else:
+#         print(form.errors)
+#     return flask.render_template('registration_creator.html', action=1, form=form)
 
 
 @blueprint.route("/logout")
--- stories_api.py
+++ stories_api.py
@@ -5,19 +5,47 @@ from wtforms.validators import DataRequired, ValidationError, EqualTo
 from flask_login import LoginManager, login_user, logout_user, current_user
 from dbremote.db_session import create_session, global_init
 from dbremote.user import User, Author
+from dbremote.storys import Story, Comment
 from main import app
 import flask
 from werkzeug.security import generate_password_hash
 
 global_init("db/data.sqlite")
-session = create_session()
 
 blueprint = flask.Blueprint('news_api', __name__,
                             template_folder='templates')
 
 
 class CommentForm(FlaskForm):
-    content = StringField("Content",validators=[DataRequired()])
+    content = StringField("Content", validators=[DataRequired()])
+    send = SubmitField("send")
 
-@blueprint.route("/feed", methods=["GET","POST"])
-def
+
+@blueprint.route("/feed", methods=["GET", "POST"])
+def feed():
+    stories_for_watching = []
+    session = create_session()
+    user = session.query(User).filter(User.id == current_user.id)
+    for i in user.followed:
+        stories_for_watching += i
+    return flask.render_template("feed.html",
+                                 stories=stories_for_watching)  # в шаблоне циклом надо идти по этой хуйне
+
+
+@blueprint.route("/story/<sid:int>")
+def story(sid):
+    session = create_session()
+    story = session.query(Story).filter(Story.id == sid)
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment()
+        comment.content = form.content.data
+        comment.head = current_user.nickname
+        session.add(comment)
+        session.commit()
+    content = story.content
+    comments = story.commented
+    return flask.render_template("story.html",content=content,comments=comments)
+
+
+
